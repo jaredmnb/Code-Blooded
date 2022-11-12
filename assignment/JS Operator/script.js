8eb749442f61936fa100a7c5ddb01cfdf0badef4
jaredmnb <80487582+jaredmnb@users.noreply.github.com>
2022-11-12 21:33:41 +0800

    assignment

---
+++ assignment/JS Operator/script.js
@@ -0,0 +1,11 @@
+let x = 10;
+
+console.log(x += 4); //14
+console.log(x -= 4); //10
+console.log(x *= 4); //40
+console.log(x /= 4); //10
+
+
+console.log(x <= 5); //false
+console.log(x >= 5); //true
+console.log(x !== 5); //true
